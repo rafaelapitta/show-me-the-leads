8ed12699145f07c55602bb68d261ccfed3f46113
Bay <rafaelapittabahiense@gmail.com>
2020-08-17 19:45:45 -0400

    mobile-version

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Landing.css';
 
+import Logo from './assets/logo-acredita.svg'
+
 function Landing() {
   return (
     <div id="landing-content">
       <div className="background">
         <header>
-          <div className="logo">
-            <h1>ACREDITA NELA$</h1>
+          <div className="logo-container">
+            <img className="logo" src={Logo} alt="Logo Acredita Nelas"/>
           </div>
         </header>
       </div>
@@ -17,7 +19,25 @@ function Landing() {
 
       </div>
       <div className="form">
+        <h1>Dê um passo à sua independencia financeira!</h1>
+        <form method="POST" action="https://smtl.gama.academy/leads/7f2b6f21-dcac-11ea-91f1-99fb0c9231dc">
         
+        <label htmlFor="Nome"></label>
+        <input
+            name="name"
+            type="text"
+            pattern="^\D{2,}\s[\D\s]{2,}$"
+            title="Digite seu nome completo"
+            required
+        />
+        <br></br>
+        <label htmlFor="Email"></label>
+        <input
+          name="email"
+          type="email"
+          required
+        />
+      </form>
       </div>
     </div>
 
