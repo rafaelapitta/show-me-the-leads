dcfa7ccb573b27b6149718cf92238a656ae94cde
Bay <rafaelapittabahiense@gmail.com>
2020-08-17 20:04:27 -0400

    structure

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Landing.css';
 
-import Logo from './assets/logo-acredita.svg'
+import Logo from './assets/logo-acredita.svg';
+import Check from './assets/check.svg';
 
 function Landing() {
   return (
@@ -15,10 +16,28 @@ function Landing() {
       </div>
     
     <div id="sections">
-      <div className="content">
+      {/* CONTENT */}
+        <div className="content">
+          <div className="title">
+            <p>Acesse o kit completo</p>
+            <h1>Dos brasileiros com dívidas, 58% são mulheres.</h1>
+          </div>
 
-      </div>
-      <div className="form">
+          <div className="are-the-one">
+            <h1>Você é uma delas?</h1>
+            <p>O Acredita Nelas surgiu da iniciativa de mulheres que identificaram a necessidade de 
+              empoderar outras mulheres através de conhecimento e ferramentas sobre a vida financeira.
+              <br></br>No nosso conteúdo você vai encontrar:</p>
+          </div>
+
+          <div className="bullet">
+            <img src={Check} alt="check"/>
+            <p>Finanças x emoções: Como nossa cabeça age no bolso?</p>
+          </div>
+        </div>
+
+      {/* FORM */}
+        <div className="form">
         <h1>Dê um passo à sua independencia financeira!</h1>
         <form method="POST" action="https://smtl.gama.academy/leads/7f2b6f21-dcac-11ea-91f1-99fb0c9231dc">
         
@@ -44,7 +63,11 @@ function Landing() {
       <input type="submit"/>
       </div>
     </div>
-
+    
+    <footer>
+      <p>All right reserved</p>
+      <a href="#landing-content">Back to Top</a>
+    </footer>
     </div>
   );
 }
