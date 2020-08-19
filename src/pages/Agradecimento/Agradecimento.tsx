c0c08c2f0cbb670a36c6fb5af8bfd38971ec88f0
Bay <rafaelapittabahiense@gmail.com>
2020-08-19 10:20:30 -0400

    final

--- src/pages/Agradecimento/Agradecimento.tsx
+++ src/pages/Agradecimento/Agradecimento.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import './index.css'
+import './index.css';
+
+import Mulher from '../../assets/mulher.png';
+
 
 function Agradecimento() {
   return (
-    <header>
-      <h1>Obrigada por baixar o nosso material</h1>
-      <img src="" alt=""/>
-    </header>
+    <div id="page">
+      <div className="title">
+        <header>
+          <h2>Obrigada por baixar <br></br>o nosso material</h2>
+          <img src={Mulher} alt="" />
+        </header>
+      
+
+      <main>
+        <a id="baixar" href="https://drive.google.com/drive/folders/1kqkYZgF-Smw6nK_uzb4fnk9SQKAGT8-2" target="blank" >BAIXAR MATERIAL</a>
+        
+        <a id="compartilhar" href="https://bit.ly/3aC6FdP" target="blank">COMPARTILHAR <br></br>NO WHATSAPP</a>
+      </main>
+      </div>
+    </div>
   )
 }
 
