c425bcde9c67c7360fa8263ba0371c77cb2c1a1e
Leise Goi <leisegoi@gmail.com>
2020-08-18 22:06:56 -0300

    Update landing css and tsx

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -4,16 +4,16 @@ import './Landing.css';
 
 import Logo from './assets/logo_acredita_thin.png';
 import Check from './assets/check.png';
+import Mulher from './assets/mulher.png'
+import Logo1 from './assets/so-braco-branco.png'
 
 function Landing() {
   return (
-    <div className="container-fluid">
-      <div className="row">
+    <div className="container-fluid h-100 pl-xs-0 pr-xs-0">
+      <div className="row h-100">
         {/* CONTENT */}
-        <div className="col-lg-7 content-col">
-          {/* <div className="logo-container">
-            
-          </div> */}
+        <div className="col-lg-7 content-col pl-lg-5 pr-lg-5">
+          
 
           <img className="logo" src={Logo} alt="Logo Acredita Nelas" />
 
@@ -48,36 +48,47 @@ function Landing() {
           </div>
 
           <div className="bonus">
-            <h4>Bonus</h4>
+            <h4>Bonus:</h4>
             <span>Três planilhas sensacionais para planejamento de orçamento e seus sonhos!</span>
           </div>
         </div>
 
         {/* FORM */}
-        <div className="col-lg-5 form-col">
-          <h3>Dê um passo à sua independencia financeira!</h3>
-          <form method="POST" action="https://smtl.gama.academy/leads/7f2b6f21-dcac-11ea-91f1-99fb0c9231dc">
+        <div className="col-lg-5 pl-lg-5 pr-lg-5 form-col">
+          <img className="logo1" src={Logo1} alt="Logo Acredita Nelas Branco" />
+          <h3 className="mb-4">Dê um passo rumo à sua independência financeira!</h3>
+          <div className='form'>
+            <form method="POST" action="https://smtl.gama.academy/leads/7f2b6f21-dcac-11ea-91f1-99fb0c9231dc">
+              <div className="form-group">
+                <input
+                  className='form-control'
+                  name="name"
+                  type="text"
+                  pattern="^\D{2,}\s[\D\s]{2,}$"
+                  title="Digite seu nome completo"
+                  placeholder='NOME COMPLETO'
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  className='form-control'
+                  name="email"
+                  type="email"
+                  title="Digite seu email"
+                  placeholder='DIGITE SEU MELHOR EMAIL'
+                  required
+                />
+              </div>
 
-            <label htmlFor="Nome"></label>
-            <input
-              name="name"
-              type="text"
-              pattern="^\D{2,}\s[\D\s]{2,}$"
-              title="Digite seu nome completo"
-              placeholder='Nome'
-              required
-            />
-            <br></br>
-            <label htmlFor="Email"></label>
-            <input
-              name="email"
-              type="email"
-              placeholder='Email'
-              required
-            />
-          </form>
-          {/* BOTÃO */}
-          <input type="submit" />
+              {/* BOTÃO */}
+              <input name="redirectTo" type="hidden" value="https://gama.academy" />
+              <input name="debugMode" type="hidden" value="true" />
+              <input className='button btn btn-block btn-success btn-lg' type="submit" value="EU QUERO MEU KIT!" />
+            </form>
+          </div>
+          {/* imagem */}
+          <img className="mx-auto d-block" src={Mulher} alt="" />
         </div>
       </div>
     </div>
